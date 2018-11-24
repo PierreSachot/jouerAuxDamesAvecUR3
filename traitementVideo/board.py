48d480436fccf365708ae82ebe5b3320355948ac
Rsisse <econstant002@ensc.fr>
2018-11-24 16:56:26 +0100

    ajout commentaire pour traitement video

--- traitementVideo/board.py
+++ traitementVideo/board.py
@@ -7,13 +7,18 @@ def get_board():
     board.light_pieces = []
     board.dark_pieces = []
     board.bitmap = [None] * 50
-
+    '''
+    En théorie les pions noirs (ceux de l'ordinateur) ne bougent pas 
+    => pas besoin de réinnitialiser dark_pieces si ca permet de simplifier le traitement vidéo
+     '''
 
     # ajout des pions sur le damier
+
+
     row = 0
     column = 1
     delta = 1
-    for _ in range(20): # nombre de pions noir
+    for _ in range(20): # nombre de pions noirs
         new_piece = DPiece(board, row, column, 'DARK')
         board.dark_pieces.append(new_piece)
         board.set_bitmap(row, column, new_piece)
@@ -27,7 +32,7 @@ def get_board():
     row = 6
     column = 1
     delta = 1
-    for _ in range(20): # nombre de pions blanc
+    for _ in range(20): # nombre de pions blancs
         new_piece = DPiece(board, row, column, 'LIGHT')
         board.light_pieces.append(new_piece)
         board.set_bitmap(row, column, new_piece)
