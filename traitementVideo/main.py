8025118526ad58eaca211b58aaea4ccfab22a106
PierreSachot <sachot.pierre@laposte.net>
2018-12-17 18:55:06 +0100

    Mise à jour du main.py pour la doc

--- traitementVideo/main.py
+++ traitementVideo/main.py
@@ -1,8 +1,22 @@
 # -*- coding:utf-8 -*-
 
-from board import get_board
-
-
+from BoardDetector import BoardDetector
+import cv2
 
 while(True):
-    print(get_board())
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    bd = BoardDetector(frame,
+                       cv2.imread(
+                           '/run/media/pierre/Partage/Documents/ENSC/2ème année/S7/Transpromo/Projet UR3/jouerAuxDamesAvecUR3/traitementVideo/Images/pattern.png'),
+                       10)
+    # On trouve le damier dans l'image
+    bd.findTemplate()
+    # On créer l'objet board.
+    board, imgBase = bd.createBoard(11, 11)
+    cv2.imshow('frame', imgBase)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
